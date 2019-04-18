ab925e495a9fa7a1b6e2178912136c3e33930da1
EmosewaPixel <awesomeggaming@gmail.com>
2019-04-18 11:51:49 +0200

    Finish with 1.13 stuff

--- src/main/java/com/EmosewaPixel/expertmodecore/world/OreGen.java
+++ src/main/java/com/EmosewaPixel/expertmodecore/world/OreGen.java
@@ -5,6 +5,7 @@ import com.EmosewaPixel.expertmodecore.blocks.trees.BlockRubberLog;
 import com.EmosewaPixel.expertmodecore.world.tree.BigRubberTreeFeatures;
 import com.EmosewaPixel.expertmodecore.world.tree.GiantTree;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.WoodedMountainsBiome;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.*;
@@ -20,21 +21,23 @@ public class OreGen {
                     new CountRangeConfig(25, 0, 0, 64), DimensionType.OVERWORLD
             ));
 
-
             biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, new DimensionFeature<>(
                     Feature.MINABLE, new MinableConfig(MinableConfig.IS_ROCK, BlockRegistry.SILVER_ORE.getDefaultState(), 9), Biome.COUNT_RANGE,
                     new CountRangeConfig(10, 0, 0, 64), DimensionType.OVERWORLD
             ));
-
 
             biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, new DimensionFeature<>(
                     Feature.MINABLE, new MinableConfig(MinableConfig.IS_ROCK, BlockRegistry.TIN_ORE.getDefaultState(), 9), Biome.COUNT_RANGE,
                     new CountRangeConfig(16, 0, 0, 64), DimensionType.OVERWORLD
             ));
 
-            if (biome.getCategory() == Biome.Category.FOREST)
+            if (biome.getCategory() == Biome.Category.FOREST || biome.getCategory() == Biome.Category.TAIGA || biome.getCategory() == Biome.Category.JUNGLE || biome.getCategory() == Biome.Category.SAVANNA || biome instanceof WoodedMountainsBiome)
                 biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, new DimensionFeature<>(
-                        Feature.RANDOM_FEATURE_LIST, new RandomDefaultFeatureListConfig(new Feature[]{new BigRubberTreeFeatures(false)}, new IFeatureConfig[]{IFeatureConfig.NO_FEATURE_CONFIG}, new float[]{0.05F}, new TreeFeature(true, 4, BlockRegistry.RUBBER_LOG.getDefaultState().with(BlockRubberLog.HAS_SAP, true), BlockRegistry.RUBBER_LEAVES.getDefaultState(), false), IFeatureConfig.NO_FEATURE_CONFIG), biome.AT_SURFACE_WITH_EXTRA, new AtSurfaceWithExtraConfig(0, 0.05F, 5), DimensionType.OVERWORLD));
+                        Feature.RANDOM_FEATURE_LIST, new RandomDefaultFeatureListConfig(new Feature[]{new BigRubberTreeFeatures(false)}, new IFeatureConfig[]{IFeatureConfig.NO_FEATURE_CONFIG}, new float[]{0.05F}, new TreeFeature(true, 4, BlockRegistry.RUBBER_LOG.getDefaultState().with(BlockRubberLog.HAS_SAP, true), BlockRegistry.RUBBER_LEAVES.getDefaultState(), false), IFeatureConfig.NO_FEATURE_CONFIG), biome.AT_SURFACE_WITH_EXTRA, new AtSurfaceWithExtraConfig(0, 0.075F, 5), DimensionType.OVERWORLD));
+
+            if (biome.getCategory() == Biome.Category.SWAMP)
+                biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, new DimensionFeature<>(
+                        Feature.RANDOM_FEATURE_LIST, new RandomDefaultFeatureListConfig(new Feature[]{}, new IFeatureConfig[]{}, new float[]{}, new TreeFeature(true, 4, BlockRegistry.RUBBER_LOG.getDefaultState().with(BlockRubberLog.HAS_SAP, true), BlockRegistry.RUBBER_LEAVES.getDefaultState(), true), IFeatureConfig.NO_FEATURE_CONFIG), biome.AT_SURFACE_WITH_EXTRA, new AtSurfaceWithExtraConfig(0, 0.075F, 5), DimensionType.OVERWORLD));
 
             if (biome.getTempCategory() == Biome.TempCategory.WARM)
                 biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, new DimensionFeature<>(
