450c03ea0cc8b23fefd31a9054e7795300f9b52b
EmosewaPixel <awesomeggaming@gmail.com>
2019-04-20 17:37:58 +0200

    Git missed one

--- src/main/java/com/EmosewaPixel/expertmodecore/items/armor/MaterialArmor.java
+++ src/main/java/com/EmosewaPixel/expertmodecore/items/armor/MaterialArmor.java
@@ -1,14 +1,20 @@
 package com.EmosewaPixel.expertmodecore.items.armor;
 
 import com.EmosewaPixel.expertmodecore.ExpertModeCore;
+import com.EmosewaPixel.expertmodecore.materials.IMaterialItem;
+import com.EmosewaPixel.expertmodecore.materials.Material;
+import com.EmosewaPixel.expertmodecore.materials.MaterialItems;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 
-public class ModArmor extends ItemArmor {
-    public ModArmor(IArmorMaterial materialIn, EntityEquipmentSlot slot) {
-        super(materialIn, slot, new Item.Properties().group(ExpertModeCore.main));
+public class MaterialArmor extends ItemArmor implements IMaterialItem {
+    private Material material;
+    private String type;
+
+    public MaterialArmor(Material material, EntityEquipmentSlot slot) {
+        super(material.getArmorMaterial(), slot, new Item.Properties().group(ExpertModeCore.main));
+        this.material = material;
         String type = "";
         switch (slot.getName()) {
             case "head":
@@ -23,6 +29,18 @@ public class ModArmor extends ItemArmor {
             case "feet":
                 type = "boots";
         }
-        setRegistryName(materialIn.toString().toLowerCase() + "_" + type);
+        this.type = type;
+        setRegistryName(material.getName() + "_" + type);
+        MaterialItems.materialItems.add(this);
+    }
+
+    @Override
+    public Material getMaterial() {
+        return material;
+    }
+
+    @Override
+    public String getItemType() {
+        return type;
     }
 }
